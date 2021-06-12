42b03a51f5524f9ba7c954b8b687d40f670206ba
ebmasters <ebmasters@gmail.com>
2021-06-12 08:30:11 -0400

    Week19.1 Activities
    Week 19.1 Activities

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,30 @@
+// Create a variable below
+var myFirstVariable = "Hello World!";
+// Console.log() your variable here.
+console.log(myFirstVariable);
+
+function addNumbers(){
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+
+    console.log(five);
+}
+
+addNumbers();
+
+function stringConcat() {
+    var taco = "taco";
+    var cat = "cat";
+    var tacoCat = taco + cat;
+    console.log(tacoCat);
+}
+
+stringConcat();
+
+
+$("h1").css("color","red");
+$(".blue").css("color","blue");
+$("#addMe").html('Hello');
+
+
